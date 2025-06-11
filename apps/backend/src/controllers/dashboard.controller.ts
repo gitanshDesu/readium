b435a6f3d73bbaa51143bd686b4f358c409f61e7
Gitansh Manchanda <manchandagitansh4@gmail.com>
2025-06-11 17:20:49 +0530

    (feat): Complete dashboard controller
    dashborad controller will help us in getting user stats to complete their dashboards

--- apps/backend/src/controllers/dashboard.controller.ts
+++ apps/backend/src/controllers/dashboard.controller.ts
@@ -1,4 +1,8 @@
+import { Blog } from "@readium/database/blog.model";
+import { Following } from "@readium/database/following.model";
+import { Like } from "@readium/database/like.model";
 import { User, UserDocumentType } from "@readium/database/user.model";
+import { CustomApiResponse } from "@readium/utils/customApiResponse";
 import { CustomError } from "@readium/utils/customError";
 import { tryCatchWrapper } from "@readium/utils/tryCatchWrapper";
 import { Request, Response } from "express";
@@ -8,16 +12,54 @@ interface CustomRequest extends Request {
   user?: NonNullable<UserDocumentType>;
 }
 
-//Get an Entire User Profile (Total Likes, Total Followers, Total Blogs, Bookmarks, BlogHistory, Followers, Following, comments, replies, likes) [But make sure only user can access their entire profile(i.e. bookmarks,blogHistory as well)]
+//using this end points we can get stats required to display on user's dashboard
 
-export const getUserProfile = tryCatchWrapper<CustomRequest>(
-  async (req: CustomRequest, res: Response) => {}
+//Get an Entire User Profile Stats (Total Likes, Total Followers, Total Blogs, Total Bookmarks, BlogHistory, Total Followers, Total Followin, Total Muted Users Total Muted Topics)
+
+export const getUserProfileStats = tryCatchWrapper<CustomRequest>(
+  async (req: CustomRequest, res: Response) => {
+    const userDetails = {
+      username: req.user?.username,
+      firstName: req.user?.firstName,
+      lastName: req.user?.lastName,
+      avatar: req.user?.avatar,
+    };
+    const TotalFollowers = await Following.countDocuments({
+      following: { $eq: req.user?._id },
+    });
+    const TotalAuthorsFollowed = await Following.countDocuments({
+      follower: { $eq: req.user?._id },
+    });
+    const TotalBookmarks = req.user?.bookmarks.length;
+    const TotalBlogs = await Blog.countDocuments({
+      author: { $eq: req.user?._id },
+    });
+    //calculate total likes on blogs
+    const TotalBlogLikes = await Like.countDocuments({
+      blog: { $in: await Blog.find({ author: req.user?._id }).distinct("_id") },
+    });
+    //TODO: Add totalBlogViews, TotalMutedUsers and TotalMutedTopics later.
+    return res.status(200).json(
+      new CustomApiResponse(
+        200,
+        {
+          userDetails,
+          TotalFollowers,
+          TotalAuthorsFollowed,
+          TotalBookmarks,
+          TotalBlogs,
+          TotalBlogLikes,
+        },
+        "User Profile Fetched Successfully!"
+      )
+    );
+  }
 );
 
-//using this end point we can get data to create user dashboard (for another user in this case)
-export const getAnotherUserProfile = tryCatchWrapper<CustomRequest>(
+//using this end point we can get stats to create user dashboard (for another user in this case)
+export const getAnotherUserProfileStats = tryCatchWrapper<CustomRequest>(
   async (req: CustomRequest, res: Response) => {
-    const { userId } = req.query;
+    const { userId } = req.params;
     //1. check if userId sent is ValidObjectId or not
     if (!isValidObjectId(userId)) {
       return res
@@ -30,12 +72,44 @@ export const getAnotherUserProfile = tryCatchWrapper<CustomRequest>(
       return res.status(404).json(new CustomError(404, "User Doesn't Exist!"));
     }
     //3. Get User details - firstName lastName username email avatar, Get User Blogs, Get User Total Followers, Total No. of People they're following
+    const userDetails = {
+      username: existingUser?.username,
+      firstName: existingUser?.firstName,
+      lastName: existingUser?.lastName,
+      avatar: existingUser?.avatar,
+    };
+    const TotalFollowers = await Following.countDocuments({
+      following: { $eq: existingUser?._id },
+    });
+    const TotalAuthorsFollowed = await Following.countDocuments({
+      follower: { $eq: existingUser?._id },
+    });
+    const TotalBookmarks = existingUser?.bookmarks.length;
+    const TotalBlogs = await Blog.countDocuments({
+      author: { $eq: existingUser?._id },
+    });
+    //calculate total likes on blogs
+    const TotalBlogLikes = await Like.countDocuments({
+      blog: {
+        $in: await Blog.find({ author: existingUser?._id }).distinct("_id"),
+      },
+    });
+    //TODO: Add totalBlogViews later.
+    return res.status(200).json(
+      new CustomApiResponse(
+        200,
+        {
+          userDetails,
+          TotalFollowers,
+          TotalAuthorsFollowed,
+          TotalBookmarks,
+          TotalBlogs,
+          TotalBlogLikes,
+        },
+        "User Profile Fetched Successfully!"
+      )
+    );
 
     //4. Return recent blogs (display blog in descending order (neweset first)) and popular(find by Views > Threshold && Likes > Threshold ) blogs (P.S we have to write some kind of algo to seggregate popular blogs from) by User
   }
-);
-
-//Get User Account Stats - Total Likes, Total Followers, Total Views on Blogs,Total Blogs
-export const getUserAccountStats = tryCatchWrapper<CustomRequest>(
-  async (req: CustomRequest, res: Response) => {}
 );
